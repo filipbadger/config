e3bacf9723f21b06bfee9a43a80713ca000402db
Filip Wiśniewski <filip@badgermapping.com>
2024-09-25 17:16:21 +0200

    Add flush all command for redis

--- setup_aliases.sh
+++ setup_aliases.sh
@@ -9,8 +9,13 @@ alias fpush="git push -f"
 alias push="git push"
 alias main="git switch main"
 alias pull="git pull"
+alias oneline="git log --oneline"
+alias commit="git commit"
+alias add-all="git add *"
 
 # navigation
 alias go='cd ~/PycharmProjects/badger-go'
 alias drupe='cd ~/PycharmProjects/badger-go/frontends/drupe-mo'
 
+# containers
+alias redis-flushall="docker exec -it badger-redis-go redis-cli FLUSHALL"
